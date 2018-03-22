94edf467af380ac82d1093a01418d894be53c3d2
TIL-MICE <1264503017@qq.com>
2018-03-22 21:34:27 +0800

    更新启动入口

--- src/main/java/com/lckiss/app/Application.java
+++ src/main/java/com/lckiss/app/Application.java
@@ -1,10 +1,18 @@
 package com.lckiss.app;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
 /**
  * 启动方式二 使用SpringBootApplication 官方demo
  */
 
-/* 使用App的那个启动方式 这里注释掉
+//使用App的那个启动方式 这里注释掉
 
 @SpringBootApplication
 public class Application {
@@ -29,4 +37,3 @@ public class Application {
     }
 
 }
-*/
